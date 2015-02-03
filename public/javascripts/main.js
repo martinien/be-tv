fb706ca4dedf777800727ab77bb59f7e69aa3463
blabla4 <amaury.cannesson@gmail.com>
2015-02-03 15:00:28 +0100

    format

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,96 +1,83 @@
-$.fn.slideFadeToggle  = function(speed, easing, callback) {
-        return this.animate({opacity: 'toggle', height: 'toggle'}, speed, easing, callback);
+$.fn.slideFadeToggle = function(speed, easing, callback) {
+    return this.animate({
+        opacity: 'toggle',
+        height: 'toggle'
+    }, speed, easing, callback);
 };
 
-
-$( document ).ready(function() {
-
-
+$(document).ready(function() {
 
     var notification = $(".notification"),
-    entrance = $("#entrance"),
-    socket = io(),
-    accept = $('#accept'),
-    refuse = $('#refuse'),
-    vlc = document.getElementById("vlc");
+        entrance = $("#entrance"),
+        socket = io(),
+        accept = $('#accept'),
+        refuse = $('#refuse'),
+        vlc = document.getElementById("vlc");
 
-    
-
-    $( "body" ).keydown(function( event ) {
+    $("body").keydown(function(event) {
         console.log("aaa");
-        if ( event.which == 39 && !refuse.hasClass("button-selected")) {
+        if (event.which == 39 && !refuse.hasClass("button-selected")) {
             //flèche droite
             switchToRefuse();
-        }
-        else if(event.which == 37 && !accept.hasClass("button-selected")){
+        } else if (event.which == 37 && !accept.hasClass("button-selected")) {
             //flèche gauche
             switchToAccept();
-        }
-        else if(event.which == 13){
+        } else if (event.which == 13) {
             validateChoice();
-        }
-        else{
+        } else {
             return;
         }
 
     });
 
+    function triggerBell() {
 
-
-    function triggerBell(){
-
-        if(entrance.attr('src') !== undefined) {
+        if (entrance.attr('src') !== undefined) {
             entrance.removeAttr('src');
-        }
-        else {
+        } else {
             entrance.attr('src', 'http://cs.isen.fr/camera/mjpg/video.mjpg');
         }
-      notification.slideFadeToggle(200, function(e){
+        notification.slideFadeToggle(200, function(e) {
             accept.addClass('button-selected');
-      });
+        });
 
     }
 
+    function triggerFireAlarm() {
 
-    function triggerFireAlarm(){
+    }
 
-      
-}
-
-
-    accept.click(function(e){
+    accept.click(function(e) {
         triggerBell();
         alert("Ouverture de la porte");
     });
-    refuse.click(function(e){
+
+    refuse.click(function(e) {
         triggerBell();
         alert("Porte fermée");
     });
 
-
-    socket.on('bellRing', function (data){
+    socket.on('bellRing', function(data) {
         console.log("sonette activée")
         triggerBell();
     });
 
-    socket.on('cmd', function (data){
+    socket.on('cmd', function(data) {
         //data = id de la commande
-        if(data == 16) {
+        if (data == 16) {
             vlc.playlist.playItem(1);
         }
-        if(data == 2064) {
+        if (data == 2064) {
             vlc.playlist.playItem(2);
         }
-        if(data == 1040) {
+        if (data == 1040) {
             vlc.playlist.playItem(3);
         }
     });
 
-    socket.on('alarm', function (data){
+    socket.on('alarm', function(data) {
 
     });
-
-
 
     vlc.video.aspectRatio = "16:9";
     var tf1 = vlc.playlist.add("dvb-t://frequency=554000000:bandwidth=0", "tf1", "program=1281");
@@ -98,34 +85,29 @@ $( document ).ready(function() {
     var m6 = vlc.playlist.add("dvb-t://frequency=554000000:bandwidth=0", "m6", "program=1283");
     vlc.playlist.add("dvb-t://frequency=594000000:bandwidth=0", "grdLilleTv", "");
 
-
-
-
-    function switchToRefuse(){
+    function switchToRefuse() {
         accept.removeClass("button-selected");
         refuse.addClass("button-selected");
     }
 
-    function switchToAccept(){
+    function switchToAccept() {
         refuse.removeClass("button-selected");
         accept.addClass("button-selected");
     }
 
-    function validateChoice(){
+    function validateChoice() {
         triggerBell();
-       getActiveButton() === accept ? socket.emit("ouverturePorte") : socket.emit("fermeturePorte");
+        getActiveButton() === accept ? socket.emit("ouverturePorte") : socket.emit("fermeturePorte");
     }
 
+    //socket.emit("ouverturePorte");
+    //socket.emit("fermeturePorte");
 
-//socket.emit("ouverturePorte");
-//socket.emit("fermeturePorte");
-
-    function getActiveButton(){
+    function getActiveButton() {
         return accept.hasClass("button-selected") ? accept : refuse;
     }
 
 });
-
 
 function play(id) {
     var vlc = document.getElementById("vlc");
