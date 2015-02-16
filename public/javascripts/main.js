5cc3b7d32f311e9ab6745426a327e44c3c13821d
blabla4 <amaury.cannesson@gmail.com>
2015-02-16 15:27:11 +0100

    widget meteo

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -29,6 +29,39 @@ function soap(CMD) {
 }
 
 $( document ).ready(function() {
+      $.simpleWeather({
+        woeid: '608105', //2357536
+        location: '',
+        unit: 'c',
+        success: function(weather) {
+          var date = new Date();
+          html = '<h3>'+weather.city+'</h3>';
+          html += '<h4>'+date.getDate()+'/'+(parseInt(date.getMonth())+1)+'/'+date.getFullYear()+' '+date.getHours()+':'+date.getMinutes()+'</h4>'
+          html += '<h2><i class="icon-'+weather.code+'"></i>';
+          html += weather.temp+'&deg;'+weather.units.temp+'</h2>';
+          html += '<table>';
+          html += '<tr>';
+          for(var i=0;i<weather.forecast.length;i++) {
+            html += '<td>'+weather.forecast[i].day+'</td>';
+          }
+          html += '</tr>';
+          html += '<tr>';
+          for(var i=0;i<weather.forecast.length;i++) {
+            html += '<td><i class="icon-'+weather.forecast[i].code+'"></i></td>';
+          }
+          html += '</tr>';
+          html += '<tr>';
+          for(var i=0;i<weather.forecast.length;i++) {
+            html += '<td>'+weather.forecast[i].high+'&deg;'+weather.units.temp+'</td>';
+          }
+          html += '</tr>';
+      	  html += '</table>';
+          $("#weather").html(html);
+        },
+        error: function(error) {
+          $("#weather").html('<p>'+error+'</p>');
+        }
+      });
 
     var notification = $("#notification"),
         entrance = $("#entrance"),
@@ -84,6 +117,7 @@ $( document ).ready(function() {
     var timer = 0;
     var timer2 = Math.floor(Date.now() / 1000);
     var nbr = "";
+    var show = 0;
     socket.on('cmd', function(data) {
             switch(data) {
                 case "1":
@@ -117,7 +151,15 @@ $( document ).ready(function() {
                         }
                         timer2 = Math.floor(Date.now() / 1000);
                     }
-                    
+
+                    break;
+                case "BLUE":
+                    if(show == 0) {
+                        $("#weather").show();
+                    } else {
+                        $("#weather").hide();
+                        show = 0;
+                    }
                     break;
                 case "GREEN":
                     if(state === STATE.BELL){
@@ -169,13 +211,13 @@ $( document ).ready(function() {
                     soap('AAAAAQAAAAEAAAAUAw==');
                     break;
             }
-                
-        
+
+
     });
 
     socket.on('alarm', function(data) {
         startBlink("65000");
-        
+
        if(state !== STATE.ALARM){
                 var previousState = state;
 
@@ -255,7 +297,7 @@ $( document ).ready(function() {
     }
 
     function closeDoor(){
-        
+
 
         jsonString = '{"hue": 0,"on": true,"bri": 10}';
         lightUrl = "http://10.134.15.60/api/2fe88a512405b6771b4ed88524094ab/lights/1/state";
