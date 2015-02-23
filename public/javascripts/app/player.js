75cf9a1ce8540b4aa9593cacb70a39da7e1ed110
blabla4 <amaury.cannesson@gmail.com>
2015-02-23 10:33:47 +0100

    clean channels

--- public/javascripts/app/player.js
+++ public/javascripts/app/player.js
@@ -15,27 +15,3 @@ define(["init"],function(init) {
     }
 
 })
-
-
-/*vlc.playlist.add("http://10.134.15.103:8866/live?channel=51");
-vlc.playlist.add("http://10.134.15.103:8866/live?channel=52");
-vlc.playlist.add("");
-vlc.playlist.add("");
-vlc.playlist.add("");
-vlc.playlist.add("http://10.134.15.103:8866/live?channel=56");
-vlc.playlist.add("http://10.134.15.103:8866/live?channel=57");
-vlc.playlist.add("http://10.134.15.103:8866/live?channel=8");
-vlc.playlist.add("http://10.134.15.103:8866/live?channel=9");
-vlc.playlist.add("");
-vlc.playlist.add("http://10.134.15.103:8866/live?channel=11");
-vlc.playlist.add("");
-vlc.playlist.add("");
-vlc.playlist.add("http://10.134.15.103:8866/live?channel=14");
-vlc.playlist.add("http://10.134.15.103:8866/live?channel=15");
-vlc.playlist.add("http://10.134.15.103:8866/live?channel=16");
-vlc.playlist.add("http://10.134.15.103:8866/live?channel=17");
-vlc.playlist.add("http://10.134.15.103:8866/live?channel=18");
-vlc.playlist.add("");
-vlc.playlist.add("http://10.134.15.103:8866/live?channel=20");
-vlc.playlist.add("http://10.134.15.103:8866/live?channel=21");
-vlc.playlist.add("http://10.134.15.103:8866/live?channel=31");*/
